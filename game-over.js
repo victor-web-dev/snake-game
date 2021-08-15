bf3d25f9547deec54d49f73985b1566ae5faa68f
wilian-tavares <wilian.tavares@souunit.com.br>
2021-08-15 13:45:07 -0300

    enviado

---
+++ game-over.js
@@ -0,0 +1,11 @@
+import { pontuacao } from './script.js';
+
+pontuacao();
+
+console.log(pontuacao())
+// document.getElementById('pontos').innerHTML = ponto;
+
+
+// function tentarNovamente() {
+//     window.location.href = 'snake-game-inicio.html';
+// }
